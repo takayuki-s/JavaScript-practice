6532ac3084d478d0b8f8da7d2f664aa300480c57
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-08 08:05:17 +0900

    型
    any型はあまり使わない

---
+++ chapter03/sample02/sample02.js
@@ -0,0 +1,5 @@
+"use strict";
+let x = 1;
+let y = 2;
+// x = "文字列を代入します"; // 型（number→string）が違うのでエラーになる
+console.log(x * y);
---
+++ chapter03/sample02/sample02.ts
@@ -0,0 +1,6 @@
+let x: number = 1;
+let y: number = 2;
+
+// x = "文字列を代入します"; // 型（number→string）が違うのでエラーになる
+
+console.log(x * y);
