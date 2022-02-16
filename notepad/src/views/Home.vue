b0d2a9e63d74c46780f148d2a6f746a546542288
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-17 07:20:05 +0900

    リスト画面を作成

--- notepad/src/views/Home.vue
+++ notepad/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-for="memo in newest" :key="memo.id">
+      <li>{{ memo.body }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  computed: {
+    newest() {
+      return this.$store.state.memos
+    }
   }
 }
 </script>
