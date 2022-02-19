2ace6d9ce3b470860b2160a5af60a286594a7181
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-20 08:26:03 +0900

    スコープチェーン
    内側のスコープから参照する
    
    1行目（スクリプトスコープ）と2行目（グローバルスコープ）の記述をしてconsole.logで2が出力されたことから、グローバルスコープはスクリプトスコープよりも外側のスコープだということが分かる

--- javascript-essence-tutorial-master/030_スコープ/040_スコープチェーン/start/main.js
+++ javascript-essence-tutorial-master/030_スコープ/040_スコープチェーン/start/main.js
@@ -1,18 +1,15 @@
 let a = 2;
 window.a = 4;
 function fn1() {
-    // let a = 1;
-    function fn2() {
-        console.log(a);
+  let a = 1;
+  function fn2() {
+    let a = 3;
+    console.log(a); // 6行目、4行目、1行目、2行目の順に参照される
 
-        if (true) {
-            var a = 3;
-        }
-
-    }
-    fn2();
+    // if (true) {
+    //   var a = 3;
+    // }
+  }
+  fn2();
 }
 fn1();
-
-
-
