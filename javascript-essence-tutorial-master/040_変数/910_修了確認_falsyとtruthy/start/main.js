491a544a8ced773e15c20ffae077f6e0486a6a5f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-25 07:24:10 +0900

    falsyとtruthyの演習問題

--- javascript-essence-tutorial-master/040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ javascript-essence-tutorial-master/040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -2,32 +2,33 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
-// console.log("0 == false", 0 == false);
-// console.log("0 === false", 0 === false);
-// console.log('"false" == false', "false" == false);
-// console.log('"0" == 0', "0" == 0);
-// console.log('Boolean("0") === false', Boolean("0") === false);
-// console.log('Boolean(0) === false', Boolean(0) === false);
-// console.log('!Boolean(0) === false', !Boolean(0) === false);
-// console.log('-1 == false', -1 == false);
-// console.log('!10 === false', !10 === false);
+
+console.log("0 == false", 0 == false); // true
+console.log("0 === false", 0 === false); // false
+console.log('"false" == false', "false" == false); //true
+console.log('"0" == 0', "0" == 0); // true
+console.log('Boolean("0") === false', Boolean("0") === false); //false
+console.log("Boolean(0) === false", Boolean(0) === false); // true
+console.log("!Boolean(0) === false", !Boolean(0) === false); // false
+console.log("-1 == false", -1 == false); // false
+console.log("!10 === false", !10 === false); // false
 
 /**
  * 問題２：
  * 関数fnの引数numが渡ってこない場合（undefinedまたはnullの場合）のみ、
  * 初期値として-1を設定したいものとします。
- * 
+ *
  * 以下の関数fnの初期化を適切に書き直してください。
  * ※aには0以上の整数値が渡ってくるものとします。
  */
 let a = 1;
 
 function fn(num) {
-    num = num || -1;
-    console.log(num);
+  let parseNum = parseInt(num);
+  parseNum = parseNum || -1;
+  console.log(parseNum);
 }
 fn(a);
-
 
 /**
  * 問題３：
@@ -39,3 +40,9 @@ fn(a);
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
+
+function greeting(name, greet = "hello") {
+  console.log(`${greet}, ${name}`);
+}
+greeting("Bob", "hi");
+greeting("Bob");
