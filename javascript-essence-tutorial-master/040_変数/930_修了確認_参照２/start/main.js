f9a498512b2f035d9451163378e31531eb5ee301
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-25 07:51:36 +0900

    参照の演習問題2

--- javascript-essence-tutorial-master/040_変数/930_修了確認_参照２/start/main.js
+++ javascript-essence-tutorial-master/040_変数/930_修了確認_参照２/start/main.js
@@ -13,8 +13,7 @@ let obj = {
 };
 
 function minus(obj, val) {
-  let prop1 = obj.prop1;
-  prop1 = prop1 - val;
+  obj.prop1 = obj.prop1 - val;
 }
 
 minus(obj, 1);
@@ -30,8 +29,7 @@ console.log(obj.prop1);
  */
 
 function double(obj) {
-  let { prop1 } = obj;
-  prop1 = prop1 * 2;
+  obj.prop1 = obj.prop1 * 2;
 }
 
 double(obj);
@@ -55,7 +53,7 @@ function fn({ prop2 }) {
   return { prop2: prop };
 }
 obj = fn(obj);
-// console.log(obj.prop2.prop3);
+console.log(obj.prop2.prop3); // 3が返ってくる prop2の参照先が関数内のpropに書き換わったから
 
 /**
  * 問題４：
@@ -67,4 +65,4 @@ function through(obj) {
 }
 
 const obj2 = through(obj);
-// console.log(obj === obj2);
+console.log(obj === obj2); // false
