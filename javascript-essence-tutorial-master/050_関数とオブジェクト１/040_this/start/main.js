fab349a2596e76603b25c51b4a063d8ef5c633a5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-27 07:20:11 +0900

    this
    実行コンテキストによって"this"の参照先は変わる

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/040_this/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,9 @@
 const person = {
-    name: 'Tom',
-    hello: function() {
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+    console.log("Hello " + person.name); // 4行目と同じ 基本的にはthisを使う
+  },
+};
 
-    }
-}
+person.hello();
