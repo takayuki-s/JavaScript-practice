b96f6e7c9f783c667c224c69882a5884e35db172
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-27 07:28:17 +0900

    参照のコピーとthis
    オブジェクトのメソッドとして実行される場合は、"this"は呼び出し元のオブジェクトを参照
    
    関数として実行される場合は、"this"はフローバルオブジェクトを参照

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,24 @@
+window.name = "John";
+
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+    a();
+    const person = {
+      name: "Tim",
+      hello: function () {
+        console.log("Hello " + this.name);
+        a();
+      },
+    };
+    person.hello();
+  },
+};
 person.hello();
+// const ref = person.hello;
+// ref();
+
+function a() {
+  console.log("Hello " + this.name);
+}
