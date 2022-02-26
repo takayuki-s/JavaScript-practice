02d481c540deb099478fb45e02f90b449d4cf606
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-27 07:39:16 +0900

    bindとthis
    bindで指定したオブジェクトがthisの参照先となる

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -1,15 +1,24 @@
-window.name = 'John';
+window.name = "John";
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+  },
+};
 person.hello();
 
+const helloTom = person.hello.bind(person);
+
 function fn(ref) {
-    ref();
+  ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+function a(name) {
+  console.log("hello " + name);
+}
+
+const b = a.bind(null, "Tim");
+b();
