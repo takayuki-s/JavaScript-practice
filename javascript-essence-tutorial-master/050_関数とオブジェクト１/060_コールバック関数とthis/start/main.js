c2eccd0fde2e522e9d01f0f41b554c01edec4678
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-27 07:30:48 +0900

    コールバック関数とthis
    関数の実行となるので、thisはフローバルオブジェクトを参照する

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
@@ -1,9 +1,15 @@
-window.name = 'John';
+window.name = "John";
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+  },
+};
 person.hello();
+
+function fn(ref) {
+  ref();
+}
+
+fn(person.hello);
