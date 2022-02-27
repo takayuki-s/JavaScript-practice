c736794fd7105d22ebb09383ce9283a32648b7d4
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-28 06:57:46 +0900

    call,applyとthis
    bind…"this"や引数の参照先を変更。使用時点で実行はしない
    
    call,apply…"this"や引数の参照先を変更。同時に関数を実行する

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,17 @@
-function a(name) {
+function a(name, name2) {
+  console.log("hello " + name + " " + name2);
   console.log("hello " + this.name);
 }
 
+const tim = { name: "Tim" };
 const b = a.bind({ name: "Tim" });
 
 b();
+
+a.apply(tim, ["Ken", "Bob"]); // この時点で関数の実行まで行われる
+a.call(tim, "Tom", "Bob"); // applyと同様この時点で関数の実行まで行われる。 第二引数以下で関数の引数を取ることができる。
+
+const arr = [1, 2, 3, 4, 5];
+// const result = Math.max.apply(null, arr); // ES6以降は以下のスプレッド演算子
+const result = Math.max(...arr);
+console.log(result);
