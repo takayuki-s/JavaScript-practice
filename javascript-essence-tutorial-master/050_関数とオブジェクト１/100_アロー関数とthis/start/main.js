c535a632e318f054e98241ecfc12714d265e275e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-02-28 07:15:05 +0900

    Update main.js
    アロー関数の"this"はレキシカルスコープの"this"を参照する

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
@@ -1,9 +1,23 @@
-window.name = 'John';
+window.name = "John";
+
+const a = () => console.log("Bye " + this.name); // thisはwindowオブジェクトを参照する（"John"）
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+    const b = () => console.log("Bye " + this.name); // thisはhello関数のオブジェクトを参照する（"Tom"）
+    a();
+    b();
+  },
+  //   hello: () => {
+  //     console.log("Hello " + this.name); // thisはwindowオブジェクトを参照する（"John"）
+  //   },
+};
 person.hello();
+
+function c() {
+  const d = () => console.log("Bye " + this.name); // thisはwindowオブジェクトを参照する（"John"）
+  d();
+}
+c();
