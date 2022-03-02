626a1e5bbca51c9392f4b6949c3b54ca6f748292
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-03 07:46:34 +0900

    自学【setTimeoutを用いた非同期処理】もう少しシンプルver

--- original/非同期処理/main.js
+++ original/非同期処理/main.js
@@ -1,16 +1,43 @@
+// const fn1 = () => {
+//   task(1);
+//   setTimeout(() => {
+//     task(2);
+//   }, 1000);
+//   task(3);
+// };
+// const fn2 = () => {
+//   task(4);
+//   setTimeout(() => {
+//     task(5);
+//   }, 1000);
+//   task(6);
+// };
+
+// const task = (order) => {
+//   console.log(`task ${order} の処理を開始`);
+//   const startTime = new Date();
+//   while (new Date() - startTime < 1000);
+//   console.log(`task ${order} の処理を終了`);
+// };
+
+// setTimeout(() => {
+//   task(0);
+// }, 1000);
+// fn1();
+// fn2();
+
 const fn1 = () => {
   task(1);
   setTimeout(() => {
     task(2);
-  }, 1000);
-  task(3);
-};
-const fn2 = () => {
-  task(4);
+  }, 3000);
   setTimeout(() => {
-    task(5);
+    task(3);
+  }, 2000);
+  setTimeout(() => {
+    task(4);
   }, 1000);
-  task(6);
+  task(5);
 };
 
 const task = (order) => {
@@ -20,8 +47,7 @@ const task = (order) => {
   console.log(`task ${order} の処理を終了`);
 };
 
-setTimeout(() => {
-  task(0);
-}, 1000);
 fn1();
-fn2();
+setTimeout(() => {
+  task(6);
+}, 1000);
