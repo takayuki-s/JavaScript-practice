402bb2ccb4cc915e94d2d7d598a5ff628deb824d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-05 07:01:00 +0900

    instanceof
    どのコンストラクターから生成されたオブジェクトかを確認する
    
    オブジェクトと配列の判断に活用できる
    
    returnでオブジェクトを返すと異なるので注意

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/040_instanceof/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,28 @@
 function F(a, b) {
-    this.a = a;
-    this.b = b;
-    // return {a: 1};
+  this.a = a;
+  this.b = b;
+  //   const result = new Object();
+  //   result.a = 1;
+  //   return result;
+  //   return {a: 1} // 上記3行の内容
 }
 
-F.prototype.c = function() {}
+F.prototype.c = function () {};
+
+const instance = new F(1, 2);
+console.log(instance);
+console.log(instance instanceof F);
+console.log(instance instanceof Object);
+console.log(instance.__proto__ === F.prototype); // 11行目
+
+function fn(arg) {
+  if (arg instanceof Array) {
+    arg.push("value");
+  } else {
+    arg["key"] = "value";
+  }
+  console.log(arg);
+}
+
+fn([]); // 配列を渡す
+fn({}); // オブジェクトを渡す
