7212af119f8d6e5d9dbe13cd66ecaaab90a2eb61
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-07 08:03:52 +0900

    自学【非同期処理のチェーンを使わずに同じ処理を書いてみる】

--- javascript-essence-tutorial-master/080_非同期処理/040_非同期処理のチェーン/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/040_非同期処理のチェーン/start/main.js
@@ -1,14 +1,35 @@
-function sleep(callback, val) {
+// function sleep(callback, val) {
+//   setTimeout(function () {
+//     console.log(val++);
+//     callback(val);
+//   }, 1000);
+// }
+
+// sleep(function (val) {
+//   sleep(function (val) {
+//     sleep(function (val) {
+//       sleep(function (val) {}, val);
+//     }, val);
+//   }, val);
+// }, 0);
+
+function fn1(val) {
   setTimeout(function () {
     console.log(val++);
-    callback(val);
+    fn2(val);
+  }, 1000);
+}
+function fn2(val) {
+  setTimeout(function () {
+    console.log(val++);
+    fn3(val);
+  }, 1000);
+}
+function fn3(val) {
+  setTimeout(function () {
+    console.log(val++);
   }, 1000);
 }
 
-sleep(function (val) {
-  sleep(function (val) {
-    sleep(function (val) {
-      sleep(function (val) {}, val);
-    }, val);
-  }, val);
-}, 0);
+fn1(0);
+console.log("hello");
