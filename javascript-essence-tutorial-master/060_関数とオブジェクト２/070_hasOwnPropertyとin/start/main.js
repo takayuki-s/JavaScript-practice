843c1cdcd0c0412118654408387d8edc7c857d16
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-06 11:33:47 +0900

    hasOwnPropertyとin
    inを使うことでプロトタイプチェーンを含めて存在を確認することができる

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,17 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
-}
+Object.prototype.hello = function () {
+  console.log("Object: hello " + this.name);
+};
 
-const bob = new Person('Bob', 18);
+const bob = new Person("Bob", 18);
+const result = bob.hasOwnProperty("name");
+const resultHello = bob.hasOwnProperty("hello");
+console.log(result); // true
+console.log(resultHello); // false
+console.log("name" in bob); // true
+console.log("hello" in bob); // true
+console.log("hasOwnProperty" in bob); // true
