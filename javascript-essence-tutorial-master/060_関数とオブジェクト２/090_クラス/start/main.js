0a92a0580b0334324469ae62c4417c2851766f56
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-07 06:03:02 +0900

    クラス
    JavaScriptにおいては、クラスで生成されたインスタンスはあくまでもオブジェクト！

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/090_クラス/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/090_クラス/start/main.js
@@ -1,8 +1,22 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log("hello " + this.name);
+  }
+}
+
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
+Person.prototype.hello = function () {
+  console.log("hello " + this.name);
+};
+
+const bob = new Person("Bob", 23);
+console.log(bob);
