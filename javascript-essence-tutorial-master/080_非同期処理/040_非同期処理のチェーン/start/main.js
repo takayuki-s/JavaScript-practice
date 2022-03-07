e0ea043893b3389bfae0d285919324c08fe913a9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-08 06:31:48 +0900

    Update main.js

--- javascript-essence-tutorial-master/080_非同期処理/040_非同期処理のチェーン/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/040_非同期処理のチェーン/start/main.js
@@ -1,10 +1,19 @@
-// function sleep(callback, val) {
+// function sleep(val) {
 //   setTimeout(function () {
 //     console.log(val++);
-//     callback(val);
+//     setTimeout(function () {
+//       console.log(val++);
+//       setTimeout(function () {
+//         console.log(val++);
+//         setTimeout(function () {
+//           console.log(val++);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
 //   }, 1000);
 // }
 
+// sleep(0);
 // sleep(function (val) {
 //   sleep(function (val) {
 //     sleep(function (val) {
@@ -13,23 +22,41 @@
 //   }, val);
 // }, 0);
 
-function fn1(val) {
-  setTimeout(function () {
-    console.log(val++);
-    fn2(val);
-  }, 1000);
-}
-function fn2(val) {
-  setTimeout(function () {
-    console.log(val++);
-    fn3(val);
-  }, 1000);
-}
-function fn3(val) {
-  setTimeout(function () {
-    console.log(val++);
-  }, 1000);
+// function fn1(val) {
+//   setTimeout(function () {
+//     console.log(val++);
+//     fn2(val);
+//   }, 1000);
+// }
+// function fn2(val) {
+//   setTimeout(function () {
+//     console.log(val++);
+//     fn3(val);
+//   }, 1000);
+// }
+// function fn3(val) {
+//   setTimeout(function () {
+//     console.log(val++);
+//   }, 1000);
+// }
+
+// fn1(0);
+// console.log("hello");
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) showOk();
+//   else showCancel();
+// }
+
+function showMessage(callback) {
+  const messageText = "showMessage Text";
+  callback(messageText);
+  showOk(messageText);
 }
 
-fn1(0);
-console.log("hello");
+function showOk(messageText) {
+  alert(messageText);
+}
+
+// 使用法: 関数 showOk, showCancel は ask の引数として渡されます
+showMessage(showOk);
