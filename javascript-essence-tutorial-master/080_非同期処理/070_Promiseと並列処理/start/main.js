36e7fc5a6ebe71c4d92398bb95c7bd2151617310
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-10 08:03:47 +0900

    Promiseと並列処理（その他様々な記法）

--- javascript-essence-tutorial-master/080_非同期処理/070_Promiseと並列処理/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -7,12 +7,18 @@ function sleep(val) {
   });
 }
 
+// // Promiseの並列処理
 Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (data) {
   console.log(data); // 各自resolveで設定した値が配列になってデータが帰ってる
 });
+
+// // 直列処理の中に並列処理
 // sleep(0)
 //   .then(function (val) {
-//     return sleep(val);
+//     return Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (val) {
+//       console.log(val);
+//       return sleep(val);
+//     });
 //   })
 //   .then(function (val) {
 //     return sleep(val);
@@ -20,3 +26,13 @@ Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (data) {
 //   .then(function (val) {
 //     return sleep(val);
 //   });
+
+// どれか1つが完了した際にthenに移る
+// Promise.race([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+//   console.log(data);
+// });
+
+// 完了した非同期処理が成功したか失敗したか分からないがとりあえず待つ
+// Promise.allSettled([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+//   console.log(data);
+// });
