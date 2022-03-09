6565296964a6bfdb5a0f078c222f7625ec1a0b93
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-10 07:49:05 +0900

    Promiseと並行処理
    Promise.all()の中に配列などで入れてあげると、全ての処理が終わるまで待ってくれる

--- javascript-essence-tutorial-master/080_非同期処理/070_Promiseと並列処理/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -1,16 +1,22 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
-    }, 1000);
+    }, val * 500);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+  console.log(data); // 各自resolveで設定した値が配列になってデータが帰ってる
+});
+// sleep(0)
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   });
