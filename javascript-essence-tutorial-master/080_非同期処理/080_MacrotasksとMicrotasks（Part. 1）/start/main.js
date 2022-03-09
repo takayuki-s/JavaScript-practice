3463baf24faee3ccf7409250e157e81369df3a77
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-10 08:36:07 +0900

    MacrotasksとMicrotasks

--- javascript-essence-tutorial-master/080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -1,12 +1,14 @@
+// Macrotasks
 setTimeout(function task1() {
-  console.log('task1');
+  console.log("task1");
 });
 
+// Microtasks
 new Promise(function promise(resolve) {
-  console.log('promise');
+  console.log("promise");
   resolve();
 }).then(function job1() {
-  console.log('job1');
+  console.log("job1");
 });
 
-console.log('global end');
+console.log("global end");
--- javascript-essence-tutorial-master/080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
@@ -1,13 +1,28 @@
 new Promise(function promise(resolve) {
-  console.log('promise');
+  console.log("promise");
 
   setTimeout(function task1() {
-    console.log('task1');
+    console.log("task1");
     resolve();
   });
-
-}).then(function job1() {
-  console.log('job1');
 })
+  .then(function job1() {
+    console.log("job1");
+    setTimeout(function task1() {
+      console.log("task2");
+      queueMicrotask(function job4() {
+        console.log("job4");
+      });
+    });
+    queueMicrotask(function job5() {
+      console.log("job5");
+    });
+  })
+  .then(function job2() {
+    console.log("job2");
+  })
+  .then(function job3() {
+    console.log("job3");
+  });
 
-console.log('global end');
+console.log("global end");
