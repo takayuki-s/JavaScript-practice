fc8e34aa24f102859d856b4b5c16545db426c052
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-11 05:43:02 +0900

    AwaitとAsync

--- javascript-essence-tutorial-master/080_非同期処理/100_AwaitとAsync/start/main.js
+++ javascript-essence-tutorial-master/080_非同期処理/100_AwaitとAsync/start/main.js
@@ -1,20 +1,40 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
     }, 1000);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  // throw new Error();
+  return val;
+}
+
+init()
+  .then(function (val) {
+    console.log("hello " + val);
+  })
+  .catch(function (e) {
+    console.error(e);
+  });
+// console.log(init()); // Promiseインスタンスを返していることが分かる
+
+// sleep(0).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
