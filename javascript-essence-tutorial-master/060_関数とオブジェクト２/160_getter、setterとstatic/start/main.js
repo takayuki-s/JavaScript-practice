20e81501dc2c0ae49b3bf6b351440eda687feb9c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-12 07:05:21 +0900

    getterとsetter

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -1,4 +1,20 @@
 function Person1(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
+
+Object.defineProperty(Person1.prototype, "name", {
+  get: function () {
+    console.log("hello get");
+    return this._name;
+  },
+  set: function (val) {
+    console.log("hello set");
+    this._name = val;
+  },
+});
+
+const p1 = new Person1("Bob", 23);
+console.log(p1.name);
+p1.name = "Tom";
+console.log(p1.name);
