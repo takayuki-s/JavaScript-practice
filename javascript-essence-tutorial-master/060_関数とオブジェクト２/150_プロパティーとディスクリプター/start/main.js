75dc1951c640695f00926d05a3b625800c74fa14
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-11 09:27:23 +0900

    プロパティーとディスクリプター
    それぞれのプロパティーにはディスクリプターが存在していて、その設定値によって挙動が変わってくる

---
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/150_プロパティーとディスクリプター/start/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// const obj = { prop: 0 };
+
+const obj = {};
+Object.defineProperty(obj, "prop", {
+  configurable: true,
+  value: 0,
+  writable: true,
+});
+
+obj.prop = 1;
+console.log(obj.prop);
+
+const descriptor = Object.getOwnPropertyDescriptor(obj, "prop");
+
+console.log(descriptor);
