21685c8b1f464ffc0bbefcf128e7fdb7a285af63
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-12 07:19:01 +0900

    getterとsetter（ES6の記法）
    インスタンス化を行わずに使用できるメソッドを静的メソッド、staticメソッドという

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -14,7 +14,30 @@ Object.defineProperty(Person1.prototype, "name", {
   },
 });
 
-const p1 = new Person1("Bob", 23);
-console.log(p1.name);
-p1.name = "Tom";
-console.log(p1.name);
+// const p1 = new Person1("Bob", 23);
+// console.log(p1.name);
+// p1.name = "Tom";
+// console.log(p1.name);
+
+class Person2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get name() {
+    console.log("hello2 get");
+    return this._name;
+  }
+  set name(val) {
+    console.log("hello2 set");
+    this._name = val;
+  }
+
+  static hello() {
+    console.log("hello"); // インスタンス化をしていないのでthisの挙動が変わる
+  }
+}
+
+const p2 = new Person2("Bob", 23);
+
+Person2.hello();
