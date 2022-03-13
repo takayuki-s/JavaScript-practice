40d8673fa6542e5346c599182c32d6001987fbc6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-14 06:38:24 +0900

    forinと列挙可能性
    definePropatyで列挙性をfalseにしても良いが、hasOwnPropertyでprototypeに格納されているものを列挙対象から外しても良い
    
    Symbolはforinの列挙対象から外れる

--- javascript-essence-tutorial-master/070_反復処理/040_forinと列挙可能性/start/main.js
+++ javascript-essence-tutorial-master/070_反復処理/040_forinと列挙可能性/start/main.js
@@ -1,5 +1,25 @@
+const s = Symbol();
 const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+  [s]: "value4",
+};
+
+Object.prototype.method = function () {};
+Object.defineProperty(Object.prototype, "method", {
+  enumerable: false,
+});
+
+const d = Object.getOwnPropertyDescriptor(Object.prototype, "method");
+const e = Object.getOwnPropertyDescriptor(obj, s);
+console.log(d);
+console.log(e);
+
+for (let key in obj) {
+  // prototypeに格納されているmethodが列挙対象から外れる
+  if (obj.hasOwnProperty(key)) {
+    console.log(key); // prop1,prop2などが出力される
+    console.log(obj[key]); // value1,value2などが出力される
+  }
 }
