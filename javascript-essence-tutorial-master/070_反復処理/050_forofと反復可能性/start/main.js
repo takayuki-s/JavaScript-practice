a3d1adce50362f5287a773fdc4952cae245e8d8b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-14 06:46:11 +0900

    forofと反復可能性

--- javascript-essence-tutorial-master/070_反復処理/050_forofと反復可能性/start/main.js
+++ javascript-essence-tutorial-master/070_反復処理/050_forofと反復可能性/start/main.js
@@ -1,1 +1,13 @@
-const arry = ['a', 'b', 'c'];
+const arry = ["a", "b", "c"];
+
+arry[4] = "e";
+
+Object.prototype.method = function () {};
+// ここでfalseにしてもforofを使った場合は列挙される
+Object.defineProperty(arry, 0, {
+  enumerable: false,
+});
+
+for (let v of arry) {
+  console.log(v); // "a", "b", "c"
+}
