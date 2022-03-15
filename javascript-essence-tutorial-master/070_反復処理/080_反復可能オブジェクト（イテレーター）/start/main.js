baaa113f1d20e87ed051445f6a7dbd83e21a731b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-16 06:47:43 +0900

    反復処理/080_反復可能オブジェクト（イテレーター）（実用非推奨）
    objectのprototypeでは通常の開発では推奨されていないので注意

--- javascript-essence-tutorial-master/070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ javascript-essence-tutorial-master/070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,10 +1,26 @@
-const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
-}
+const items = {
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+};
 
-const items = Object.entries(obj);
-for (let item of items) {
-	console.log(item);
+Object.prototype[Symbol.iterator] = function () {
+  const keys = Object.keys(this);
+  console.log(keys);
+  let i = 0;
+  let _this = this;
+  return {
+    next() {
+      let key = keys[i++];
+      return {
+        value: [key, _this[key]],
+        done: i > keys.length,
+      };
+    },
+  };
+};
+
+// const items = Object.entries(obj);
+for (let [k, v] of items) {
+  console.log(k, v);
 }
