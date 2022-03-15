a6c50154d7bb2c7ee8315817f097ae70e1ed4ab6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-16 06:59:25 +0900

    ジェネレーター（イテレーターとの違い）
    ジェネレーター自体が反復可能オブジェクトを返している

--- javascript-essence-tutorial-master/070_反復処理/090_ジェネレーター/start/main.js
+++ javascript-essence-tutorial-master/070_反復処理/090_ジェネレーター/start/main.js
@@ -1,10 +1,36 @@
-function* gen() {
-  yield 1;
-  yield 2;
-  return 3;
+// function* gen() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
+
+// const it = gen();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+function* genIterator(max = 10) {
+  let i = 0;
+  while (i < max) {
+    yield i++;
+  }
+  return; // 省略可
 }
 
-const it = gen();
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
+const it = genIterator(10);
+
+let a = it.next();
+while (!a.done) {
+  console.log(a.value);
+  a = it.next();
+}
+
+const obj = {
+  [Symbol.iterator]: genIterator,
+};
+for (let i of obj) {
+  console.log(i);
+}
+for (let i of genIterator()) {
+  console.log(i);
+}
