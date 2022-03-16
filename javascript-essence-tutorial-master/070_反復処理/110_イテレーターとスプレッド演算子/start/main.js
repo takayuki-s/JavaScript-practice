a1ac8f4d965f7c7b54a3a110425f22dac58443c6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-17 05:44:25 +0900

    イテレーターとスプレッド演算子

---
+++ javascript-essence-tutorial-master/070_反復処理/110_イテレーターとスプレッド演算子/start/main.js
@@ -0,0 +1,30 @@
+const arry1 = [1, 2, 3, 4, 5];
+const arry2 = [...arry1, 6];
+console.log(arry2);
+console.log(arry1 === arry2); // false
+
+function sum(...args) {
+  let ret = 0;
+  for (let v of args) {
+    console.log(v);
+    ret += v;
+  }
+  return ret;
+}
+const result = sum(1, 2, 3, 4);
+console.log(result);
+
+const obj1 = {
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+};
+
+Object.prototype[Symbol.iterator] = function* () {
+  for (let key in this) {
+    yield [key, this[key]];
+  }
+};
+
+const arry3 = [...obj1];
+console.log(arry3);
