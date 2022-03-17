92d4679762d8cc1a8ab7893de0e527bc449ced81
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-18 05:57:30 +0900

    PubSubパターンの演習問題

--- javascript-essence-tutorial-master/070_反復処理/930_修了確認_PubSubパターン/start/main.js
+++ javascript-essence-tutorial-master/070_反復処理/930_修了確認_PubSubパターン/start/main.js
@@ -4,17 +4,18 @@
  */
 
 function customFn1() {
-	console.log('customFn1');
+  console.log("customFn1");
 }
 function customFn2() {
-	console.log('customFn2');
+  console.log("customFn2");
 }
 function customFn3() {
-	console.log('customFn3');
+  console.log("customFn3");
 }
 
-events.on('beforeInit', customFn1);
-events.on('beforeInit', customFn2);
-events.on('afterInit', customFn3);
+events.on("beforeInit", customFn1);
+events.off("beforeInit", customFn1);
+events.on("beforeInit", customFn2);
+events.on("afterInit", customFn3);
 
 new MyLibrary();
