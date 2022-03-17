55a67146a67734be6f835676b9fa7afd40e86eb2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-18 06:44:54 +0900

    ESModuleと即時関数
    moduleでexportしたプリミティブの型の値の変更はESmoduleでは許可されない←オブジェクトで格納するか、関数呼び出しで変更するかといった方法を使う

--- javascript-essence-tutorial-master/090_モジュラーJavaScript/020_ESModuleと即時関数/start/main.js
+++ javascript-essence-tutorial-master/090_モジュラーJavaScript/020_ESModuleと即時関数/start/main.js
@@ -1,20 +1,26 @@
 const moduleA = (function () {
-
-  console.log('IIFE called');
+  console.log("IIFE called");
 
   let privateVal = 1;
   let publicVal = 10;
 
   function publicFn() {
-    console.log('publicFn called: ' + publicVal);
+    console.log("publicFn called: " + publicVal);
   }
 
-  function privateFn() {
-
-  }
+  function privateFn() {}
 
   return {
     publicFn,
-    publicVal
-  }
+    publicVal,
+  };
 })();
+
+const moduleB = (function ({ publicFn: fn, publicVal: val }) {
+  fn();
+  fn();
+  fn();
+  console.log(val++);
+  console.log(val++);
+  fn();
+})(moduleA);
---
+++ javascript-essence-tutorial-master/090_モジュラーJavaScript/020_ESModuleと即時関数/start/moduleB.js
@@ -0,0 +1,8 @@
+import { publicFn as fn, publicVal as val } from "./moduleA.js";
+
+fn();
+fn();
+fn();
+console.log(val.prop++);
+console.log(val.prop++);
+fn();
