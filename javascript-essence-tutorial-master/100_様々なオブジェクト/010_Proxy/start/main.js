f22e99eeeaeb47eaeabb0b72455e4194c743cd9b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-19 06:35:51 +0900

    Proxy（応用）

--- javascript-essence-tutorial-master/100_様々なオブジェクト/010_Proxy/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/010_Proxy/start/main.js
@@ -2,19 +2,28 @@ const targetObj = { a: 0 };
 const handler = {
   set: function (target, prop, value, receiver) {
     console.log(`[set]: ${prop}`);
-    target[prop] = value;
+    // target[prop] = value;
+    throw new Error("cannot add prop"); // プロパティを変更させないようにする
   },
   get: function (target, prop, receiver) {
-    console.log(`[get]: ${prop}`);
-    return target[prop];
+    console.log(receiver);
+    // デフォルト値の設定
+    if (target.hasOwnProperty(prop)) {
+      console.log(`[get]: ${prop}`);
+      return target[prop];
+    } else {
+      return "-1";
+    }
   },
   deleteProperty: function (target, prop) {
     console.log(`[delete]: ${prop}`);
-    delete target[prop];
+    // delete target[prop];
+    throw new Error("cannot delete prop"); // プロパティを削除させないようにする
   },
 };
 
 const pxy = new Proxy(targetObj, handler);
+console.log(pxy.b);
 pxy.a = 1;
 pxy.a;
 delete pxy.a;
