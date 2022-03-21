90d434e9a84029a9f86cbd12f22aa7d3d90b1225
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-22 07:02:08 +0900

    配列part3（reduce）
    配列のそれぞれの値を1つにまとめる
    
    何らかの処理を加える時は初期値を入れると良い

--- javascript-essence-tutorial-master/100_様々なオブジェクト/100_Array（配列）３/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/100_Array（配列）３/start/main.js
@@ -1,1 +1,16 @@
 const arry = [1, 2, 3, 4, 5];
+
+const result = arry.reduce(function (accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+}, 0);
+
+console.log(result); // 15
+
+// 文字列はどうなるか検証
+const stringArry = ["a", "b", "c"];
+const reduceArry = stringArry.reduce(function (accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+});
+console.log(reduceArry);
