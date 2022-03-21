48a7f4a3a17151e1d4fbaa8bb6a7e49cabfd0bbc
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-22 06:52:59 +0900

    配列part2

--- javascript-essence-tutorial-master/100_様々なオブジェクト/090_Array（配列）２/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/090_Array（配列）２/start/main.js
@@ -1,1 +1,17 @@
 const arry = [1, 2, 3, 4, 5];
+
+arry.forEach(function (v, i, arry) {
+  console.log(v);
+});
+
+const newArry = arry.map(function (v, i, arry) {
+  console.log(v);
+  return v * 2;
+});
+console.log(newArry);
+
+const filterArry = arry.filter(function (v, i, arry) {
+  return i >= 1; // truthyな値のみ配列に格納
+});
+
+console.log(filterArry);
