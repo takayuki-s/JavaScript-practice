22e98db1df146d14df3a657ec9dabfa3acf02950
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-23 07:47:51 +0900

    データストアの演習問題part2

--- javascript-essence-tutorial-master/100_様々なオブジェクト/940_修了確認_データストア２/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/940_修了確認_データストア２/start/main.js
@@ -2,41 +2,49 @@
  * 問題：
  * オブジェクトの状態をlocalStorageに登録する処理を
  * 非同期としてください。
- * 
+ *
  * また、複数回同じプロパティーに対して更新が入った場合
  * にも、localStorageへの登録は１回のみとなるように変更してください。
- * 
+ *
  */
-const KEY = 'test-data';
+const KEY = "test-data";
+let _dirty;
 
 class DataSource {
-	static getLocal(KEY) {
-		console.log('get from local');
-		const result = localStorage.getItem(KEY);
-		return JSON.parse(result);
-	}
+  static getLocal(KEY) {
+    console.log("get from local");
+    const result = localStorage.getItem(KEY);
+    return JSON.parse(result);
+  }
 
-	static setLocal(KEY, target) {
-		console.log('set to local');
-		const json = JSON.stringify(target);
-		localStorage.setItem(KEY, json);
-	}
+  static setLocal(KEY, target) {
+    console.log("set to local");
+    const json = JSON.stringify(target);
+    localStorage.setItem(KEY, json);
+  }
 }
 
 const targetObj = DataSource.getLocal(KEY) || {};
 
 const pxy = new Proxy(targetObj, {
-	set(target, prop, value, receiver) {
-		const result = Reflect.set(target, prop, value, receiver);
+  set(target, prop, value, receiver) {
+    _dirty = true;
+    const result = Reflect.set(target, prop, value, receiver);
 
-		DataSource.setLocal(KEY, target);
-		
-		return result;
-	}
+    Promise.resolve().then(() => {
+      if (_dirty) {
+        console.log("** update data **");
+        _dirty = false;
+        DataSource.setLocal(KEY, target);
+      }
+    });
+
+    return result;
+  },
 });
 
-console.log('init', pxy);
-pxy.name = 'Tom';
-console.log('change', pxy);
-pxy.name = 'Tim';
-console.log('change2', pxy);
+console.log("init", pxy);
+pxy.name = "Tom";
+console.log("change", pxy);
+pxy.name = "Tim";
+console.log("change2", pxy);
