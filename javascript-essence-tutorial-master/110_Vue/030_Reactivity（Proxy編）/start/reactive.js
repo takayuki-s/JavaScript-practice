49b8a6961c4f817c7a23a41a3f71d43128ac472b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-24 07:37:50 +0900

    Reactivity（Proxy）

--- javascript-essence-tutorial-master/110_Vue/030_Reactivity（Proxy編）/start/reactive.js
+++ javascript-essence-tutorial-master/110_Vue/030_Reactivity（Proxy編）/start/reactive.js
@@ -1,3 +1,30 @@
+const handler = {
+  get(target, key, receiver) {
+    const res = Reflect.get(target, key, receiver);
+    console.log(
+      "%c[reactive:get]",
+      "background: green; color: white",
+      key,
+      res
+    );
+    return res;
+  },
+  set(target, key, value, receiver) {
+    const res = Reflect.set(target, key, value, receiver);
+    console.log(
+      "%c[reactive:get]",
+      "background: red; color: white",
+      key,
+      value
+    );
+    trigger();
+    return res;
+  },
+};
+
+function reactive(target) {
+  return new Proxy(target, handler);
+}
 let activeEffect = null;
 function effect(fn) {
   activeEffect = fn;
@@ -5,4 +32,4 @@ function effect(fn) {
 function trigger() {
   activeEffect();
 }
-export { effect, trigger };
+export { effect, reactive };
