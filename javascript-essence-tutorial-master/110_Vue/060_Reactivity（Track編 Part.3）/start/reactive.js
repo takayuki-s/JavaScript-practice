ec4275e4f23f580b846effb18116e4a0dd570422
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-25 07:26:20 +0900

    コード整形

--- javascript-essence-tutorial-master/110_Vue/060_Reactivity（Track編 Part.3）/start/reactive.js
+++ javascript-essence-tutorial-master/110_Vue/060_Reactivity（Track編 Part.3）/start/reactive.js
@@ -1,17 +1,27 @@
 const handler = {
   get(target, key, receiver) {
     const res = Reflect.get(target, key, receiver);
-    console.log('%c[reactive:get]', 'background: green; color: white;', key, res);
+    console.log(
+      "%c[reactive:get]",
+      "background: green; color: white;",
+      key,
+      res
+    );
     track(target, key);
     return res;
   },
   set(target, key, value, receiver) {
     const res = Reflect.set(target, key, value, receiver);
-    console.log('%c[reactive:set]', 'background: red; color: white;', key, value);
+    console.log(
+      "%c[reactive:set]",
+      "background: red; color: white;",
+      key,
+      value
+    );
     trigger(target);
     return res;
-  }
-}
+  },
+};
 function reactive(target) {
   return new Proxy(target, handler);
 }
@@ -24,10 +34,15 @@ function effect(fn) {
 
 const targetMap = new WeakMap();
 function track(target, key) {
-  console.log('%c[effect:register]', 'background: blue; color: white;', target, activeEffect);
+  console.log(
+    "%c[effect:register]",
+    "background: blue; color: white;",
+    target,
+    activeEffect
+  );
   let depsMap = targetMap.get(target);
-  
-  if(!depsMap) {
+
+  if (!depsMap) {
     targetMap.set(target, (depsMap = new Map()));
   }
 
