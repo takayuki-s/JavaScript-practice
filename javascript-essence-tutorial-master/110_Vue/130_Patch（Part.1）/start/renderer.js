220ffc92d2d47dfe647a9ce64bad4e413810d048
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-27 07:24:34 +0900

    Patch part1
    差分を検知して変更

---
+++ javascript-essence-tutorial-master/110_Vue/130_Patch（Part.1）/start/renderer.js
@@ -0,0 +1,24 @@
+import { nodeOps } from "./nodeOps.js";
+
+function createVNode(type = "", props = {}, children = "") {
+  return {
+    type,
+    props,
+    children,
+  };
+}
+
+function patch(n1, n2, container) {
+  console.log(n1);
+  console.log(n2);
+  let el;
+  if (n1.type !== n2.type) {
+    el = nodeOps.create(n2.type);
+    nodeOps.append(container, el);
+  }
+  if (n1.children !== n2.children) {
+    nodeOps.html(el, n2.children);
+  }
+}
+
+export { createVNode, patch };
