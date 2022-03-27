56bff69588250e44590e6aa5cc30e6edb8161967
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-28 07:23:12 +0900

    Patch part3

--- javascript-essence-tutorial-master/110_Vue/150_Patch（Part.3）/start/renderer.js
+++ javascript-essence-tutorial-master/110_Vue/150_Patch（Part.3）/start/renderer.js
@@ -1,38 +1,48 @@
-import { nodeOps } from './nodeOps.js';
+import { nodeOps } from "./nodeOps.js";
 
-function createVNode(type = '', props = {}, children = '') {
+function createVNode(type = "", props = {}, children = "") {
   return {
     type,
     props,
-    children
-  }
+    children,
+  };
 }
 
 function patch(n1, n2, container) {
-  console.log(n1, n2);
   let el;
-  if(n1.type !== n2.type) {
-    el = nodeOps.create(n2.type);
+  if (n1.type !== n2.type) {
+    el = n2.el = nodeOps.create(n2.type);
     nodeOps.append(container, el);
+  } else {
+    el = n2.el = n1.el;
   }
 
-  for(const key in n2.props) {
+  for (const key in n2.props) {
     const prevProp = n1.props[key];
     const nextProp = n2.props[key];
 
-    if(prevProp !== nextProp) {
-      if(key.startsWith('on')) {
+    if (prevProp !== nextProp) {
+      if (key.startsWith("on")) {
         nodeOps.on(el, key.substring(2).toLowerCase(), () => {
           nextProp();
         });
       } else {
         nodeOps.setAttr(el, key, nextProp);
       }
-      
     }
   }
-  if(n1.children !== n2.children) {
-    nodeOps.html(el, n2.children);
+  if (n2.children instanceof Array) {
+    for (let i = 0; i < n2.children.length; i++) {
+      if (n1.children.hasOwnProperty(i)) {
+        patch(n1.children[i], n2.children[i], el);
+      } else {
+        patch(createVNode(), n2.children[i], el);
+      }
+    }
+  } else {
+    if (n1.children !== n2.children) {
+      nodeOps.html(el, n2.children);
+    }
   }
 }
 
