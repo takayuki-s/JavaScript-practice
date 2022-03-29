e27c117ac7780353e67bc9cf0731a173cb31ce9e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-30 06:34:20 +0900

    簡易アプリ作成 Part5
    ・+-ボタンクリックで数字の増減が行われる

--- javascript-essence-tutorial-master/110_Vue/230_FinalApp（Part.5）/start/app.js
+++ javascript-essence-tutorial-master/110_Vue/230_FinalApp（Part.5）/start/app.js
@@ -1,31 +1,37 @@
-import { nodeOps } from './nodeOps.js';
-import { createVNode, patch } from './renderer.js';
-import { reactive, computed, effect } from './reactive.js';
+import { nodeOps } from "./nodeOps.js";
+import { createVNode, patch } from "./renderer.js";
+import { reactive, computed, effect } from "./reactive.js";
 function createApp(args) {
-    const { data, computed: computedData, render } = args;
+  const { data, computed: computedData, methods, render } = args;
 
-    const app = {};
+  const app = {};
 
-    app.data = reactive(data());
+  app.methods = methods;
 
-    app.computed = {};
-    for(const prop in computedData) {
-        const c = computed(computedData[prop], app);
-        app.computed[prop] = c;
-    }
+  app.data = reactive(data());
 
-    app.mount = function(selector) {
-        const container = nodeOps.qs(selector);
-        app.vnode = createVNode();
-        effect(() => {
-            const nextVNode = render.call(app);
-            console.log('%c[patch]', 'background: orange; color: white;', app.vnode, nextVNode);
-            patch(app.vnode, nextVNode, container);
-            app.vnode = nextVNode;
-        })
-        
-    }
-    return app;
+  app.computed = {};
+  for (const prop in computedData) {
+    const c = computed(computedData[prop], app);
+    app.computed[prop] = c;
+  }
+
+  app.mount = function (selector) {
+    const container = nodeOps.qs(selector);
+    app.vnode = createVNode();
+    effect(() => {
+      const nextVNode = render.call(app);
+      console.log(
+        "%c[patch]",
+        "background: orange; color: white;",
+        app.vnode,
+        nextVNode
+      );
+      patch(app.vnode, nextVNode, container);
+      app.vnode = nextVNode;
+    });
+  };
+  return app;
 }
 
 export { createApp, createVNode as h };
