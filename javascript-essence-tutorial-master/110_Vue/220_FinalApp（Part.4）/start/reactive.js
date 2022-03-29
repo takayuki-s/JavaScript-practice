ae629dad34ed96a0127ffac44409a7b3fa821819
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-03-30 06:16:00 +0900

    簡易アプリ作成 Part4
    ・computedの実装

--- javascript-essence-tutorial-master/110_Vue/220_FinalApp（Part.4）/start/reactive.js
+++ javascript-essence-tutorial-master/110_Vue/220_FinalApp（Part.4）/start/reactive.js
@@ -1,17 +1,29 @@
 const handler = {
   get(target, key, receiver) {
     const res = Reflect.get(target, key, receiver);
-    console.log('%c[reactive:get]', 'background: green; color: white;', target, key, res);
+    console.log(
+      "%c[reactive:get]",
+      "background: green; color: white;",
+      target,
+      key,
+      res
+    );
     track(target, key);
     return res;
   },
   set(target, key, value, receiver) {
     const res = Reflect.set(target, key, value, receiver);
-    console.log('%c[reactive:set]', 'background: red; color: white;', target, key, value);
+    console.log(
+      "%c[reactive:set]",
+      "background: red; color: white;",
+      target,
+      key,
+      value
+    );
     trigger(target, key);
     return res;
-  }
-}
+  },
+};
 function reactive(target) {
   return new Proxy(target, handler);
 }
@@ -48,7 +60,13 @@ function track(target, key) {
   }
 
   if (!deps.has(activeEffect)) {
-    console.log('%c[effect:register]', 'background: blue; color: white;', target, key, activeEffect);
+    console.log(
+      "%c[effect:register]",
+      "background: blue; color: white;",
+      target,
+      key,
+      activeEffect
+    );
     deps.add(activeEffect);
   }
 }
@@ -62,8 +80,8 @@ function trigger(target, key) {
   if (!deps) {
     return;
   }
-  deps.forEach(effect => {
-    if(effect.computed) {
+  deps.forEach((effect) => {
+    if (effect.computed) {
       effect.dirty = true;
     } else {
       effect();
@@ -71,21 +89,25 @@ function trigger(target, key) {
   });
 }
 
-function computed(getter) {
+function computed(getter, ctx) {
   let computed, value;
-  const runner = effect(getter, { computed: true });
+  const runner = effect(getter.bind(ctx), { computed: true });
 
   computed = {
     get value() {
       if (runner.dirty) {
         value = runner();
         runner.dirty = false;
-        console.log('%c[computed:refresh]', 'background: purple; color: white;', value);
+        console.log(
+          "%c[computed:refresh]",
+          "background: purple; color: white;",
+          value
+        );
       }
-      
+
       return value;
-    }
-  }
+    },
+  };
   return computed;
 }
 export { effect, trigger, reactive, computed };
