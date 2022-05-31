3d7db2a315fbfec7f083dadda363f2e6487e10c4
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-01 07:33:08 +0900

    レスト構文

--- original/Javascript_Typescript/main.js
+++ original/Javascript_Typescript/main.js
@@ -10,3 +10,11 @@ const person = {
 
 const personResult = { ...person, height: 180 };
 console.log(personResult);
+
+const arr = [1, 2, 3, 4, 5];
+const [a, b, ...c] = arr;
+console.log(a, b, c);
+
+const obj = { a: 1, b: 2, c: 3, x: 10 };
+const { x, ...props } = obj;
+console.log(x, props);
